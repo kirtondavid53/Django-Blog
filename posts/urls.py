24f29322b52d41ce260603ec17374faf2b187266
kirtondavid53 <kirtondavid53@gmail.com>
2023-07-09 17:46:00 -0400

    add django form

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
           path('', views.index, name='index'),
           path('post/<str:pk>', views.post, name='post'),
-          path('login', views.login_view, name='login')
-            ]
+          path('login', views.login_view, name='login'),
+          path('add_post', views.add_post, name='add_post')
+          ]
