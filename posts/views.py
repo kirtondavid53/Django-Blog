d7b90133007f28a3e749e13b1bb83e30f145320f
kirtondavid53 <kirtondavid53@gmail.com>
2023-07-10 14:34:13 -0400

    add update and delete

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import PostForm
 
@@ -55,4 +56,32 @@ def add_post(request):
     else:
         messages.info(request, "You must logged in to add a post")
         return redirect('index')
+    
+
+@login_required(login_url="login")
+def update_post(request, pk):
+    post = Post.objects.get(id=pk)
+ 
+    form = PostForm(request.POST or None, instance=post)  
+    if form.is_valid():  
+        try:  
+            form.save() 
+            model = form.instance
+            messages.info(request, "Your post has been updated")
+            return redirect('index')  
+        except Exception as e: 
+            messages.info(request, "There was an error. Please try again")   
+            return redirect('update_post') 
+    return render(request,'update_post.html',{'form':form})  
+
+
+@login_required(login_url="login")
+def delete_post(request, pk):
+
+    post = Post.objects.get(id=pk)
+
+    post.delete()
+
+    messages.info(request, "Your post has been deleted")
+    return redirect('index')
         
