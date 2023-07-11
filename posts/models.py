d54554fb07f3e7bab34cc3a2e3653a03e6d27d57
kirtondavid53 <kirtondavid53@gmail.com>
2023-07-10 21:16:05 -0400

    add author and image to post

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length= 50, default='')
-    body = models.TextField(max_length= 1000000, default='')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # body = models.TextField(max_length= 1000000, default='')
+    body = RichTextField(blank=True, null=True)
+    snippet = models.CharField(max_length=50, default='Click to read blog post')
     date_created = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return (f'{self.title}')
 
 # Create your models here.
--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
           path('', views.index, name='index'),
           path('post/<str:pk>', views.post, name='post'),
           path('login', views.login_view, name='login'),
+          path('logout', views.logout_view, name='logout'),
           path('add_post', views.add_post, name='add_post'),
           path('update_post/<int:pk>', views.update_post, name='update_post'),
           path('delete_post/<int:pk>', views.delete_post, name='delete_post')
